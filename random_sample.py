c8489fb35111ec2068e2bb2368adf96bc058031c
Zhuoheng Li <40489953+Andy-LZH@users.noreply.github.com>
2019-09-05 20:49:00 +0800

    Update random_sample.py

--- random_sample.py
+++ random_sample.py
@@ -1,10 +1,11 @@
 import numpy as np
 import random
-# This first part of the program is desinged to create a random sample from 1 to 7, and the second will calculate all sample's(sample [1,1000]) x bar and you can compare it with the real population M.
+# create a random sample from 1 to 7
 
 def create_random_sample():
     return np.random.randint(1, 7)
 
+# calculate all sample's(sample [1,1000]) x bar and you can compare it with the population mean
 
 def calculate_random_samples():
     sample = 0.0
